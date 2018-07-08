fbc6786de3aa28ea91362b0a6685432d5ddd3087
Erik (Mac) <git@kire.sk>
2018-07-08 20:03:23 +0200

    OtherTest.php - class for misc tests - testPermissions - test access to directory /storage and /bootstrap/cache --php-test

---
+++ tests/Unit/OtherTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+
+/**
+ * Class OtherTest
+ * Ostatne univerzalne testy
+ * @package Tests\Unit
+ */
+class OtherTest extends TestCase {
+
+    /**
+     * Test pristupovych prav
+     * Ak test spadne, nie su pristupne (zapis) alebo neexistuju adresare, ktore aplikacia potrebuje ku spravnemu fungovaniu
+     */
+    public function testPermissions()
+    {
+        $this->assertDirectoryIsWritable('../../storage/', 'Directory /storage is not writable or does not exists');
+        $this->assertDirectoryIsWritable('../../bootstrap/cache/', 'Directory /bootstrap/cache is not writable or does not exists');
+    }
+}
