7a2f9e8baecfb7dc756964840d3c587ec5017544
Erik (Mac) <git@kire.sk>
2018-07-08 16:13:52 +0200

    testing test --php-test DalsiTest

---
+++ tests/Unit/DalsiTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DalsiTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertFalse(false);
+    }
+
+    public function testBasicDalsi()
+    {
+        $this->assertEquals('a', 'ba');
+    }
+}
