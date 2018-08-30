376bdba746fdbdca7f5dd25127eef15d9fc6e499
Erik Poliak <kire@kire.sk>
2018-08-30 17:50:52 +0200

    Tasks system (test database)

--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,40 @@
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+//Route::get('/', 'HomeController@index')->name('home');
+
+Route::get('/', function () {
+    $tasks = \App\Task::orderBy('created_at', 'asc')->get();
+
+    return view('front.tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+/**
+ * Add A New Task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new \App\Task();
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/task/{id}', function ($id) {
+    //
+});
